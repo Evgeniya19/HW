0995d4720fcbcdc16c3fc7371118626e26ddd50e
Evgeniya19 <32125849+Evgeniya19@users.noreply.github.com>
2017-09-20 12:50:38 +0300

    Add files via upload

---
+++ Helloworld.cpp
@@ -0,0 +1,19 @@
+// Helloworld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+	_getch();
+
+    return 0;
+}
+
